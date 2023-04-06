cc05cf11d4ac7da781c4c42b33c80616774ac5c9
SofianAcDev <sofian.achbabi@gmail.com>
2023-04-06 19:18:38 +0200

    add filter, sort & click events

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+const rangeValue = document.getElementById("rangeValue");
 const countriesContainer = document.querySelector(".countries-container");
 const inputSearch = document.getElementById("inputSearch");
+const inputRange = document.getElementById("inputRange");
+const btnSort = document.querySelectorAll(".btnSort");
+console.log(btnSort);
+
+const minToMax = document.getElementById("minToMax");
+const maxToMin = document.getElementById("maxToMin");
+const alpha = document.getElementById("alpha");
+
+console.log(minToMax);
 let countriesData = [];
+let sortMethod = "alpha";
 
 async function FetchCountries() {
   await fetch("https://restcountries.com/v3.1/all")
@@ -17,6 +28,18 @@ function countriesDisplay() {
         .toLowerCase()
         .includes(inputSearch.value.toLowerCase())
     )
+    .sort((a, b) => {
+      if (sortMethod === "maxToMin") {
+        return b.population - a.population;
+      } else if (sortMethod === "minToMax") {
+        return a.population - b.population;
+      } else if (sortMethod === "alpha") {
+        return a.translations.fra.common.localeCompare(
+          b.translations.fra.common
+        );
+      }
+    })
+    .slice(0, inputRange.value)
     .map(
       (country) =>
         `
@@ -33,20 +56,16 @@ function countriesDisplay() {
     .join("");
 }
 
-//Input value
 inputSearch.addEventListener("input", countriesDisplay);
+inputRange.addEventListener("input", () => {
+  countriesDisplay();
+  rangeValue.textContent = inputRange.value;
+});
 
+btnSort.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    sortMethod = e.target.id;
+    countriesDisplay();
+  });
+});
 window.addEventListener("load", FetchCountries);
-
-// 2 - Créer une fonction pour "fetcher" les données, afficher les données dans la console.
-
-// 3 - Passer les données à une variable
-
-// 4 - Créer une fonction d'affichage, et paramétrer l'affichage des cartes de chaque pays grace à la méthode MAP
-
-// 5 - Récupérer ce qui est tapé dans l'input et filtrer (avant le map) les données
-// coutry.name.includes(inputSearch.value);
-
-// 6 - Avec la méthode Slice gérer le nombre de pays affichés (inputRange.value)
-
-// 7 - Gérer les 3 boutons pour trier (méthode sort()) les pays
